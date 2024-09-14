f25df7c02cc08ce6be5873a0b760730225de5005
mangaorphy <o.manga@alustudent.com>
2024-09-14 20:00:27 +0200

    updated for pep8

--- models/base_model.py
+++ models/base_model.py
@@ -30,20 +30,20 @@ class BaseModel:
             self.__dict__[key] = val
         if 'created_at' in kwargs:
             self.created_at = datetime.strptime(
-                    kwargs['created_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'],
+                '%Y-%m-%dT%H:%M:%S.%f')
         if 'updated_at' in kwargs:
             self.updated_at = datetime.strptime(
-                    kwargs['updated_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['updated_at'],
+                '%Y-%m-%dT%H:%M:%S.%f')
 
     def __str__(self):
         """overide str representation of self"""
         fmt = "[{}] ({}) {}"
         return fmt.format(
-                type(self).__name__,
-                self.id,
-                self.__dict__)
+            type(self).__name__,
+            self.id,
+            self.__dict__)
 
     def save(self):
         """updates last updated variable"""
--- models/engine/errors.py
+++ models/engine/errors.py
@@ -8,6 +8,7 @@ Defines the custome errors used in File Storage
 
 class ModelNotFoundError(Exception):
     """Raised when an unknown module is passed"""
+
     def __init__(self, arg="BaseModel"):
         super().__init__(f"Model with name {arg} is not registered!")
 
@@ -17,4 +18,4 @@ class InstanceNotFoundError(Exception):
 
     def __init__(self, obj_id="", mod="BaseModel"):
         super().__init__(
-                f"Insatnce of {mod} with id {obj_id} does not exist!")
+            f"Insatnce of {mod} with id {obj_id} does not exist!")
